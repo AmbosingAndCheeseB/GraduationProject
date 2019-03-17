6b52ebca5e55e0d0227aeb9bca574d79ac5c035c
CheeseB <2489ckckck@naver.com>
2019-03-17 11:57:26 +0900

    크롤러 틀

--- Crawler.py
+++ Crawler.py
@@ -1,14 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
+
 def get_html(url):
     _html = ""
     resp = requests.get(url)
     if resp.status_code == 200:
         _html = resp.text
     return _html
-
-
 
 
 web_url = 'https://www.google.com/'
