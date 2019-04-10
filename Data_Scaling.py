5a8a654558649abb4d5601c90349f036c2b643d4
CheeseB <2489ckckck@naver.com>
2019-04-10 14:31:41 +0900

    modify

--- Data_Scaling.py
+++ Data_Scaling.py
@@ -1,10 +1,11 @@
 from sklearn.preprocessing import MinMaxScaler
 from openpyxl import load_workbook
+from openpyxl import Workbook
 
 load_wb = load_workbook("./Train_Data.xlsx", data_only=True)
 load_ws = load_wb['Train_Data']
 
-get_cells = load_ws['B1':'B5919']
+get_cells = load_ws['G1':'G5919']
 value_list = []
 
 for row in get_cells:
@@ -13,4 +14,14 @@ for row in get_cells:
 
 scale = MinMaxScaler(feature_range=(0.001, 0.999))
 normal = scale.fit_transform(value_list)
-print(normal)
+
+write_wb = Workbook()
+write_ws = write_wb.active
+
+i = 1
+for row in normal:
+    for value in row:
+        write_ws.cell(i, 1, value)
+        i = i+1
+
+write_wb.save('./Normal_Data.xlsx')
