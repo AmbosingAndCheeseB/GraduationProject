ae5e6192f5ed8f368d20e8596bd9495debd20fe6
CheeseB <2489ckckck@naver.com>
2019-04-16 23:58:35 +0900

    modified

--- prediction_max_power.py
+++ prediction_max_power.py
@@ -1,10 +1,11 @@
 import tensorflow as tf
 import numpy as np
 import pandas as pd
+import os
 from matplotlib import pyplot as plt
 
 # 랜덤으로 정해도 시드를 정하면 어떤 시드의 가중치가 제일 좋은지 알 수 있음
-tf.set_random_seed(77)
+tf.set_random_seed(100)
 
 x_data = pd.read_csv('./Train_X.csv')
 y_data = pd.read_csv('./Train_Y.csv')
@@ -20,7 +21,7 @@ label_test = y_test.as_matrix().astype('float32')
 
 # 초기값들 설정
 learning_rate = 0.0005
-num_epoch = 10000
+num_epoch = 20000
 batch_size = 100
 display_step = 10
 hidden1_size = 5
@@ -52,41 +53,60 @@ def model_ANN(x):
     return logit
 
 
-pre_value=model_ANN(x)
+pre_value = model_ANN(x)
 
 loss = tf.reduce_mean(tf.square(y-pre_value))
 train = tf.train.RMSPropOptimizer(learning_rate=learning_rate).minimize(loss)
 
+saver_dir = "pre_model"
+saver = tf.train.Saver()
+ckpt_path = os.path.join(saver_dir, "model")
+ckpt = tf.train.get_checkpoint_state(saver_dir)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
 
+    ckpt_question = input("파라미터를 불러오시겠습니까?(Y/y) ")
+
+    if ckpt_question in "y" or ckpt_question in "Y":
+        if ckpt and ckpt.model_check_path:
+            saver.restore(sess, ckpt.model_check_path)
+        else:
+            print("저장된 가중치 파라미터가 읍슴다")
+
     for epoch in range(num_epoch):
-        average_loss = 0
+
         sess.run(iterator.initializer, feed_dict={x: feature})
 
         while True:
             try:
                 next_x, next_y = sess.run(next_data)
 
-                _, current_loss = sess.run([train, loss], feed_dict={x:next_x, y:next_y})
+                _, current_loss = sess.run([train, loss], feed_dict={x: next_x, y: next_y})
 
             except tf.errors.OutOfRangeError:
                 break
 
         if epoch % display_step == 0:
-            print("Epoch : %d, loss : %f" % ((epoch+1), current_loss))
+            print("Epoch : %d, loss : %f" % ((epoch + 1), current_loss))
 
-    pre_val, y1 = sess.run([pre_value, y], feed_dict={x: feature_test, y: label_test})
+        if epoch % 1000 == 0:
+            saver.save(sess, ckpt_path, global_step=epoch)
+
+        pre_val, y1 = sess.run([pre_value, y], feed_dict={x: feature_test, y: label_test})
 
     row = np.arange(1, 101)  # 그래프 가로축 범위
     col1 = []
     col2 = []  # 그래프값 저장할 리스트
 
+    avg = 0
     for i in range(100):
         print("예측값 : %d, 실제 값 : %d" % (pre_val[i], y1[i]))
         col1.extend(pre_val[i])
         col2.extend(y1[i])  # 그래프 리스트에 결과값 추가
+        avg = avg + abs(y1[i] - pre_val[i]) / y1[i]  # 평균 오차율
+
+    print("평균 오차율 : %0.2f %%" % avg)
 
     # 그래프 그리기 #
     plt.plot(row, col1, 'r', label='Predict')
