111ae8048141c18cffe869bcad1d86534facbb2f
CheeseB <2489ckckck@naver.com>
2019-10-24 16:23:49 +0900

    modify

--- TOEIC problem_BERT/OCR/MainActivity.java
+++ TOEIC problem_BERT/OCR/MainActivity.java
@@ -1,18 +1,23 @@
 package com.example.graduation;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.googlecode.tesseract.android.TessBaseAPI;
 
@@ -26,16 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
     TessBaseAPI tessBaseAPI;
 
-    Button button;
     CameraSurfaceView surfaceView;
+    Button button;
     TextView textView;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        surfaceView = findViewById(R.id.surfaceView);
+        imageView = findViewById(R.id.imageView);
         textView = findViewById(R.id.textView);
 
         button = findViewById(R.id.button);
@@ -50,6 +56,46 @@ public class MainActivity extends AppCompatActivity {
         String dir = getFilesDir() + "/tesseract";
         if(checkLanguageFile(dir+"/tessdata"))
             tessBaseAPI.init(dir, "eng");
+
+        requestPermissionCamera();
+    }
+
+    public boolean requestPermissionCamera(){
+        int sdkVersion = Build.VERSION.SDK_INT;
+        if(sdkVersion >= Build.VERSION_CODES.M) {
+
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(MainActivity.this,
+                        new String[]{Manifest.permission.CAMERA}, 0);
+
+            }else {
+                surfaceView = findViewById(R.id.surfaceView);
+            }
+        }else{  // version 6 이하일때
+            surfaceView = findViewById(R.id.surfaceView);
+            return true;
+        }
+
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        if (0 == requestCode) {
+
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // 권한 허가시
+                surfaceView = findViewById(R.id.surfaceView);
+            } else {
+                // 권한 거부시
+            }
+            return;
+        }
+
     }
 
     boolean checkLanguageFile(String dir)
@@ -104,6 +150,8 @@ public class MainActivity extends AppCompatActivity {
                 Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
                 bitmap = GetRotatedBitmap(bitmap, 90);
 
+                imageView.setImageBitmap(bitmap);
+
                 button.setEnabled(false);
                 button.setText("텍스트 인식중...");
                 new AsyncTess().execute(bitmap);
@@ -138,7 +186,6 @@ public class MainActivity extends AppCompatActivity {
 
         protected void onPostExecute(String result) {
             textView.setText(result);
-            Toast.makeText(MainActivity.this, ""+result, Toast.LENGTH_LONG).show();
 
             button.setEnabled(true);
             button.setText("텍스트 인식");
